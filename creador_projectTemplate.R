97c082594fec13dcdc5c91afd2f0bdb7b65a6d26
Sebastian Jaremczuk <jarem.seba@gmail.com>
2020-03-24 12:27:38 -0300

    se modifico el creador de proyecto para que funcione

--- creador_projectTemplate.R
+++ creador_projectTemplate.R
@@ -1,12 +1,12 @@
-pathrel = getSrcDirectory()[1]
-setwd(pathrel)
+pathrel = getwd()
+pathrel = paste0(pathrel,"/Analisis_desercion_en_ingenieria")
 library(ProjectTemplate)
-options(ProjectTemplate.templatedir = paste0(pathrel)
-create.project(paste0(pathrel, 'desercion', template= 'R_template_proyecto_base')
+options(ProjectTemplate.templatedir = paste0(pathrel))
+create.project(paste0(pathrel, '/desercion'), template= 'R_template_proyecto_base')
 
 getOption('ProjectTemplate.templatedir')
 
-setwd(pastes0(pathrel,"desercion")
+setwd(paste0(pathrel,"/desercion"))
 getwd()
 
 ###########################################################
