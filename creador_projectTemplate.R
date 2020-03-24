f96387775da76779a282b5deeb31375596a8a827
Sebastian Jaremczuk <jarem.seba@gmail.com>
2020-03-24 12:42:28 -0300

    comento linea donde agrego un nivel en el path porque el docker se va a lanzar 1 nivel mas profundo para compartir unicamente 1 solo repositorio que es donde se trabajara

--- creador_projectTemplate.R
+++ creador_projectTemplate.R
@@ -1,5 +1,5 @@
 pathrel = getwd()
-pathrel = paste0(pathrel,"/Analisis_desercion_en_ingenieria")
+#pathrel = paste0(pathrel,"/Analisis_desercion_en_ingenieria")
 library(ProjectTemplate)
 options(ProjectTemplate.templatedir = paste0(pathrel))
 create.project(paste0(pathrel, '/desercion'), template= 'R_template_proyecto_base')
