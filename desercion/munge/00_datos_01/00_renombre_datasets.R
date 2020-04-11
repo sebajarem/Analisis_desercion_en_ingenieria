7dd0326b9a006fce0f23d4c7c24783fcbec4d621
Sebastian Jaremczuk <jarem.seba@gmail.com>
2020-04-11 13:58:13 -0300

    renombre de datasets

---
+++ desercion/munge/00_datos_01/00_renombre_datasets.R
@@ -0,0 +1,9 @@
+# renombro los datasets porque los nombres que pone projecttemplate son largos
+
+cursada = X00.datos.01.Cursadas.Alumnos.SIGA.Hoja1
+alumnos = X00.datos.01.Datos.Alumnos.SIGA.Datos.Alumnos
+finales = X00.datos.01.Finales.Alumnos.SIGA.Finales
+
+rm(X00.datos.01.Cursadas.Alumnos.SIGA.Hoja1, X00.datos.01.Datos.Alumnos.SIGA.Datos.Alumnos, X00.datos.01.Finales.Alumnos.SIGA.Finales)
+gc()
+
