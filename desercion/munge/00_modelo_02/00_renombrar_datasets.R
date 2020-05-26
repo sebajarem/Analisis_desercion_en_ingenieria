cde0d4eb50e98dce291c51966738c4dcb6700e00
Sebastian Jaremczuk <jarem.seba@gmail.com>
2020-05-26 00:49:03 -0300

    formateo para trabajar. preprocesado de datos y seleccion de variables mas importantes

---
+++ desercion/munge/00_modelo_02/00_renombrar_datasets.R
@@ -0,0 +1,9 @@
+# renombro los datasets porque los nombres que pone projecttemplate son largos
+
+datos_train = X00.modelo.02.baseline.2009.train
+datos_test = X00.modelo.02.baseline.2009.test
+
+rm(X00.modelo.02.baseline.2009.train, 
+   X00.modelo.02.baseline.2009.test)
+gc()
+
